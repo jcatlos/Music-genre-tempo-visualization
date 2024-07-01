47f27f10415a9a137d5c8b10f21016b9ca40c8fa
jcatlos <jakub.catlos@gmail.com>
2024-07-01 12:16:29 +0200

    README, documentation, repo fixing up

---
+++ README.md
@@ -0,0 +1,19 @@
+# Music tempo vizualization
+Explore the tempo of different genres. See how they look in comparison. Here are all music genres from the [FMA dataset](https://github.com/mdeff/fma/tree/master).
+
+![vizualization_loop](viz-loop.gif)
+
+
+## Drill-down into the data
+Click on a genre from thi inner circle to zoom in. click into the centre to zoom out.
+
+![vzooming_in_and_out](drill-down.gif)
+
+## About
+The visualization is done using [d3.js](https://d3js.org/). It was done by using [this template](https://observablehq.com/@d3/zoomable-sunburst) as a beseline.
+
+
+## Documentation
+The visualization runs solely from the `songs.html` file. It downloads the file `genre_data.json` from a *Github gist*. The file `genre_data.json` is in the repo only for demonstration - editing it will not result in any change of the visualization.
+
+The `genre_data.json` is created by the script `data_prep.py`. It requires files `data/echonest.csv`, `data/genres.csv` and `data/tracks.csv` (files `echonest.csv`, `genres.csv` and `tracks.csv` from the [FMA dataset](https://github.com/mdeff/fma/tree/master), all placed into a folder `data`). 
--- data_prep.py
+++ data_prep.py
@@ -3,13 +3,12 @@ import numpy as np
 import pandas as pd
 import json
 
+# Load files
 tracks = tracks = pd.read_csv('data/tracks.csv', index_col=0, header=[0, 1])
 echonest = pd.read_csv('data/echonest.csv', index_col=0, header=[0, 1, 2])
 genres = pd.read_csv('data/genres.csv', index_col=0).sort_values(by=['parent'], ascending=True)
 
 # Create song data:
-    # song_tempo  = echonest[('audio_features', 'tempo')].copy()
-    # song_genres = tracks[('track', 'genres')].copy()
 song_data = pd.DataFrame(
     data = {
         'bpm': echonest[('echonest', 'audio_features', 'tempo')].copy(),
@@ -18,10 +17,20 @@ song_data = pd.DataFrame(
 )
 song_data.dropna(inplace=True)
 
+
+"""
+Having a list of genres as a string: "[1, 2, 4, 6] and a genre id, determine, whether the id is in the list." 
+"""
 def contains_genre(genre_list:str, genre_id: int) -> bool:
     genres: List[int] = eval(genre_list)
     return (genre_id in genres)
 
+"""
+Find aggregated information about a genre.
+
+Aggregated info is the number of the songs with the genre and sum of their BPMs.
+As this data is to be propagated further up in the hierarchy, we cannot compute the average yet.
+"""
 def get_genre_data(genre_id: int) -> Tuple[int, float]:
     songs_filter = song_data['genres'].apply(lambda g_list: contains_genre(g_list, genre_id))
     songs_filtered = song_data[songs_filter]
@@ -31,6 +40,12 @@ def get_genre_data(genre_id: int) -> Tuple[int, float]:
 
     return (int(song_count), float(bpm_sum))
 
+
+"""
+Process genre node. Return dictionary of attributes.
+
+Recursively called on node's children. Need to be called only on the top-level nodes.
+"""
 def process_node(idx, parent_idx):
     id = idx
     name = genres.loc[idx, 'title']
@@ -39,8 +54,18 @@ def process_node(idx, parent_idx):
     children = [process_node(child_id, idx) for child_id in children_nodes.index]
     children = [child for child in children if child != None]
 
+    children_count = sum(child['track_count_agg'] for child in children)
+    children_bpm   = sum(child['bpm_sum_agg'] for child in children)
+
     track_count, bpm_sum = get_genre_data(id)
+
+    # Print processing data
     print(f'genre data for {name}: count={track_count}; bpm={bpm_sum}')
+
+    # If the node has children, append an additional node with the parent's data.
+    # The parent data does not hold BPM 
+        # 1. the visuzlization looks less jagged
+        # 2. some songs have only the parent genre
     if track_count > 0 and len(children) > 0:
         children.append(
             {
@@ -55,10 +80,8 @@ def process_node(idx, parent_idx):
             }
         )
 
-    children_count = sum(child['track_count_agg'] for child in children)
-    children_bpm   = sum(child['bpm_sum_agg'] for child in children)
-
-    if len(children) == 0 and :
+    # Only leaf nodes hold not aggregated data
+    if len(children) == 0 and track_count > 0:
         return {
             'name': name,
             'id' : id,
@@ -70,6 +93,7 @@ def process_node(idx, parent_idx):
             'bpm_sum_agg': bpm_sum
         }
 
+    # Parent nodes hold only aggregated data
     elif track_count + children_count > 0:
         return {
             'name': name,
@@ -82,16 +106,14 @@ def process_node(idx, parent_idx):
             'bpm_sum_agg': children_bpm
         }
 
+    # If genre has no tracks or sub-genres, throw it out
     else:
         return None
 
 
+########################## MAIN ##############################
+
 if __name__ == '__main__':
-
-    
-
-    # print(song_data.info())
-    # get_genre_data(514)
 
     # Create hierarchy
     top_nodes = genres[genres['parent'] == 0]
@@ -101,11 +123,10 @@ if __name__ == '__main__':
     children_count = sum(child['track_count_agg'] for child in children)
     children_bpm   = sum(child['bpm_sum_agg'] for child in children)
     
-
-
     print(f'tracks = {children_count}[{type(children_count)}]')
     print(f'bpm  = {children_bpm}[{type(children_bpm)}]')
 
+    # Root node containing all top-level nodes
     genre_data = {
         'name': 'all_genres',
         'id': 0,
@@ -116,9 +137,7 @@ if __name__ == '__main__':
         'bpm_sum_agg': children_bpm
     }
 
-    # # Fill hierarchy with songs
-
-    # # Save data
+    # Save data
     with open('genre_data.json', 'w') as file:
         json.dump(genre_data, file)
 
